afb7bd6b7f39345df42a82e714627595dab98daa
CarlosLaraFP <info@carloslaraai.com>
2023-02-16 09:42:23 -0800

    Concurrent coordination primitives in ZIO using Ref

--- src/main/scala/com/rockthejvm/part4coordination/Refs.scala
+++ src/main/scala/com/rockthejvm/part4coordination/Refs.scala
@@ -52,9 +52,58 @@ object Refs extends ZIOAppDefault {
     }
   }
 
-  // TODO: Use cases
-  //   -
+  // example: distributing work
+  // not thread-safe, hard to debug, mixing pure and impure code
+  def demoConcurrentWorkImpure: UIO[Unit] = {
+    var count = 0
+
+    def task(workload: String): UIO[Unit] = {
+      val wordCount = workload.split(" ").length
+      for {
+        _ <- ZIO.succeed(s"Counting words for: $workload: $wordCount").debugThread
+        newCount <- ZIO.succeed(count + wordCount)
+        _ <- ZIO.succeed(s"New total: $newCount").debugThread
+        _ <- ZIO.succeed(count += wordCount) // update the variable
+      } yield ()
+    }
+
+    val effects = List("ZIO is init", "Ref is latest", "Scala is foundation", "Caliban is king").map(task)
+
+    ZIO.collectAllParDiscard(effects)
+  }
+  // every fiber sees its own version of the variable
+
+  def demoConcurrentWorkPure: UIO[Unit] = {
+    def task(workload: String, total: Ref[Int]): UIO[Unit] = {
+      val wordCount = workload.split(" ").length
+      for {
+        _ <- ZIO.succeed(s"Counting words for: $workload: $wordCount").debugThread
+        newCount <- total.updateAndGet(_ + wordCount)
+        _ <- ZIO.succeed(s"New total: $newCount").debugThread
+      } yield ()
+    }
+
+    for {
+      counter <- Ref.make(0)
+      _ <- ZIO.collectAllParDiscard(
+        List("ZIO is init", "Ref is latest", "Scala is foundation", "Caliban is king")
+          .map(load => task(load, counter))
+      )
+    } yield ()
+  }
+  // all fibers are blocked until the single fiber currently holding the lock releases it
+
+  // the update and modify functions may be run more than once if locked out on mutation attempt
+  def demoMultipleUpdates: UIO[Unit] = {
+    def task(id: Int, ref: Ref[Int]): UIO[Unit] =
+      ref.modify(previous => (println(s"Task $id updating Ref at $previous"), id))
+
+    for {
+      ref <- Ref.make(0)
+      _ <- ZIO.collectAllParDiscard((1 to 10).toList.map(i => task(i, ref)))
+    } yield ()
+  }
 
 
-  override def run: ZIO[Any, Any, Any] = ???
+  override def run: ZIO[Any, Any, Any] = demoMultipleUpdates
 }
